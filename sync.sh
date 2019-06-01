87221a095459773fbc355009a73abffa06db09fd
Justin Moon <mail@justinmoon.com>
2019-06-01 12:51:41 -0500

    screwing around

--- sync.sh
+++ sync.sh
@@ -1,8 +1,7 @@
 # TODO: abort the script if anything is in $NETWORK_DIRECTORY/blocks
 
-
 UTXO_DOWNLOAD_LINK="http://utxosets.blob.core.windows.net/public/utxo-snapshot-bitcoin-testnet-1445586.tar"
-BITCOIN_DATA_DIR="/home/gitpod/.bitcoin"
+BITCOIN_DATA_DIR="/workspace/exercises/.bitcoin"
 NETWORK_DIRECTORY="$BITCOIN_DATA_DIR/testnet3"
 TAR_NAME="$(basename $UTXO_DOWNLOAD_LINK)"
 TAR_FILE="$BITCOIN_DATA_DIR/$TAR_NAME"
@@ -29,3 +28,4 @@ if ! tar -xf "$TAR_FILE" -C "$BITCOIN_DATA_DIR"; then
   echo "Failed extracting, did you turned bitcoin off? (btcpay-down.sh)"
   exit 1
 fi
+
