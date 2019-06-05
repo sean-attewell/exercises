9e4cfede7e1be928f2d6a251938a59b817f22576
Justin Moon <mail@justinmoon.com>
2019-06-04 20:21:56 -0500

    alias python

--- assets/scripts/bashrc.sh
+++ assets/scripts/bashrc.sh
@@ -4,3 +4,4 @@ export BITCOIN_DATA_DIR="/workspace/bitcoin"
 alias testnet="bitcoin-cli -testnet -datadir=$BITCOIN_DATA_DIR -rpcuser=bitcoin -rpcpassword=python"
 alias testnetd="bitcoind -testnet -datadir=$BITCOIN_DATA_DIR -prune=550"
 alias mainnet='bitcoin-cli -rpcuser=bitcoin -rpcpassword=python -rpcconnect=68.183.110.103'
+alias python="python3"
